dbaa6e58ceea0fdcf3acd4eeb6016a336a54f556
Manuel <manuel.delverme@gmail.com>
2015-10-14 23:45:28 +0200

    loading stats from disk

--- train.py
+++ train.py
@@ -1,6 +1,54 @@
+# -*- coding: UTF-8 -*-
 import bob
+import re
+import numpy as np
+
+MODELS_PATH = "../jfa_models"
+LISTS_PATH = "../jfa_lists"
+
+def parse_list(file_name):
+    with open(filename) as recording:
+        lst = recording.read().split()
+    # logical, physical = zip(*[ str.split(row, "=") for row in lst ])
+    logical, physical = [ str.split(row, "=") for row in lst ]
+    return logical, physical
 
 def main():
+    m = [float(mi) for mi in  open("{}/ubm_means".format(MODELS_PATH)).read().split()]
+    v = [float(vi) for vi in  open("{}/ubm_variances".format(MODELS_PATH)).read().split()]
+    w = [float(wi) for wi in  open("{}/ubm_weights".format(MODELS_PATH)).read().split()]
+    m = np.array(m)
+    v = np.array(v)
+    w = np.array(w)
+
+    n_mixtures = len(w)
+    dim = len(m) / n_mixtures
+
+    # dim is the real width but the data is stored as (n,1)
+    m = np.reshape(m, (dim, n_mixtures))
+    v = np.reshape(m, (dim, n_mixtures))
+
+    datasets = []
+    datasets.append(['enroll_stats'])
+    datasets.append(['fa_train_eigenchannels_stats'])
+    datasets.append(['fa_train_eigenvoices_stats'])
+    datasets.append(['test_stats'])
+
+    for i in range(datasets):
+        list_file = "{}/{}.lst".format(LISTS_PATH, datasets)
+        spk_logical, spk_physical = parse_list(list_file)
+        n_sessions = len(spk_logical)
+
+        N = np.array(n_sessions, n_mixtures)
+        F = np.array(n_sessions, n_mixtures)
+
+        for session_i in range(n_sessions):
+            session_name = spk_physical[i]
+
+    print "done"
+
+""""
+# follow http://www1.icsi.berkeley.edu/Speech/presentations/AFRL_ICSI_visit2_JFA_tutorial_icsitalk.pdf
 # 1/ Features
     # voice actifvity detection
     energy/LTSD/SOX-something?
@@ -69,3 +117,10 @@ def main():
     variance_threshold = 0.0001
     relevance_factor = 4
     responsibilities_threshold = 0
+"""
+
+if "__main__" == __name__:
+    main()
+else:
+    print "imported, quitting"
+
