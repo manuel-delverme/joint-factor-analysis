de5ec67767adec75290e8961ac594e17ab9bb562
flip <noflip@bits.cc>
2015-10-22 20:21:21 +0200

    training PCA

--- train.py
+++ train.py
@@ -3,6 +3,7 @@
 from cheatcodes import Timer, plot_gmms
 from sklearn  import mixture
 import scipy.io.wavfile as wavfile
+from scipy.decomposition import PCA
 import glob
 try:
     import bob.ap
@@ -17,24 +18,24 @@ MODELS_PATH = JFA_PATH + "/models"
 LISTS_PATH = JFA_PATH + "/lists"
 
 def collect_suf_stats(data, m, v, w):
-    n_mixtures = len(w)
+    nr_mixtures = len(w)
     dim = len(m)
 
-    gammas = gaussian_posteriors(data, m, v, w)
+    gammas = gaussianr_posteriors(data, m, v, w)
     N = np.sum(gammas, 2)
     F = data * gammas
-    F = np.reshape(F, n_mixtures * dim, 1)
+    F = np.reshape(F, nr_mixtures * dim, 1)
 
-def gaussian_posteriors(data, m, v, w):
-    n_mixtures = len(w)
-    n_frame = len(data[0])
+def gaussianr_posteriors(data, m, v, w):
+    nr_mixtures = len(w)
+    nr_frame = len(data[0])
     dim = len(data)
 
-    g = mixture.GMM(n_mixtures)
+    g = mixture.GMM(nr_mixtures)
     g.fit(data)
 
-    for ii in range(n_mixtures):
-        gammas[i] = gaussian_function(data, a[ii], m[:, ii], v[:, ii])
+    for ii in range(nr_mixtures):
+        gammas[i] = gaussianr_function(data, a[ii], m[:, ii], v[:, ii])
 
     #normalize
     gammas /= sum(gammas)
@@ -52,10 +53,10 @@ def parse_list(file_name):
     return logical, physical
 
 def collect_suf_stats(data, m, v, w):
-    n_mixtures = len(w)
+    nr_mixtures = len(w)
     dim = len(m)
     
-    gammas = gaussian_posteriors(data, m, v, w)
+    gammas = gaussianr_posteriors(data, m, v, w)
 
     # zero order stats for each gaussian are just
     # the sum of the posteriors (soft counts)
@@ -63,23 +64,27 @@ def collect_suf_stats(data, m, v, w):
 
     # first order stats is jsut a posterior weighted sum
     F = data * gammas
-    np.reshape(F,(n_mixtures * dim, 1))
+    np.reshape(F,(nr_mixtures * dim, 1))
     return N, F
 
 def train_ubm(nr_mixtures, features):
     gmm = mixture.GMM(nr_mixtures)
+    #TODO should use Baum-Welch algorithm?
     gmm.fit(features)
     return gmm
 
+def train_pca(database_of_speakers):
+
+
 def extract_features(recordings_folder):
-    nr_utt_in_ubm = 300
+    nr_utt_inr_ubm = 300
     file_path = glob.glob(recordings_folder)[0]
     rate, signal = wavfile.read(file_path)
 
-    win_length_ms = 25 # The window length of the cepstral analysis in milliseconds
-    win_shift_ms = 10 # The window shift of the cepstral analysis in milliseconds
-    n_filters = 24 #NOTSURE The number of filter bands
-    n_ceps = 12 # The number of cepstral coefficients
+    winr_length_ms = 25 # The window length of the cepstral analysis in milliseconds
+    winr_shift_ms = 10 # The window shift of the cepstral analysis in milliseconds
+    nr_filters = 24 #NOTSURE The number of filter bands
+    nr_ceps = 12 # The number of cepstral coefficients
     f_min = 0. #NOTSURE The minimal frequency of the filter bank
     f_max = 4000. #NOTSURE The maximal frequency of the filter bank
     delta_win = 2 #NOTSURE The integer delta value used for computing the first and second order derivatives
@@ -89,7 +94,7 @@ def extract_features(recordings_folder):
     #TODO add feature wrapping
 
     mfcc = np.random.random((12,len(signal)))
-    # bob.ap.Ceps(rate, win_length_ms, win_shift_ms, n_filters, n_ceps, f_min, f_max, delta_win, pre_emphasis_coef, mel_scale, dct_norm)
+    # bob.ap.Ceps(rate, winr_length_ms, winr_shift_ms, nr_filters, nr_ceps, f_min, f_max, delta_win, pre_emphasis_coef, mel_scale, dct_norm)
     # signal = np.cast['float'](signal) # vector should be in **float**
     # mfcc = c(signal)
     return mfcc
@@ -97,9 +102,34 @@ def extract_features(recordings_folder):
 
 def main():
     features = extract_features("data/ubm/*")
+    speaker_factors , dataset_size = features.shape
+    nr_mixtures = speakers_factors
 
-    gmm = train_ubm(2, features)
+    #TODO check C,F
+    C = nr_mixtures
+    F = nr_mixtures * dataset_size
+    Rc = channel_factors
+    Rs = speakers_factors
+    CF = C*F
+
+    m = np.empty((CF,1))
+    u = np.empty((CF,Rc))
+    v = np.empty((CF,Rs))
+    d = np.sparse.diag_matrix((CF,CF))
+    Sigma = np.sparse.diag_matrix((CF,CF))
+    Lambda = (m. u, v, d, Sigma)
+
+    gmm = train_ubm(nr_mixtures = 2, features=features)
     plot_gmms([gmm], [features])
+
+    more_data = glob.glob("data/more_data/*")
+    pca_components = 2
+    pca = PCA(n_components=pca_components)
+    database = [extract_features(recording) for recording in more_data]
+    pca.fit(database)
+
+    #fit PCA
+
     models = {}
     speaker_names = os.listdir("data/")
     for speaker_name in speaker_names:
@@ -111,23 +141,11 @@ def main():
             models[speaker_name][i] = speaker_gmm
     print "done"
 
-    return
-
-    m = [float(mi) for mi in  open("{}/ubm_means".format(MODELS_PATH)).read().split()]
-    v = [float(vi) for vi in  open("{}/ubm_variances".format(MODELS_PATH)).read().split()]
-    w = [float(wi) for wi in  open("{}/ubm_weights".format(MODELS_PATH)).read().split()]
-    m = np.array(m)
-    v = np.array(v)
-    w = np.array(w)
-
-    UBM = train_ubm(nr_mixtures = 3)
-
-    n_mixtures = len(w)
-    dim = len(m) / n_mixtures
+    dim = len(m) / nr_mixtures
 
     # dim is the real width but the data is stored as (n,1)
-    m = np.reshape(m, (dim, n_mixtures))
-    v = np.reshape(m, (dim, n_mixtures))
+    m = np.reshape(m, (dim, nr_mixtures))
+    v = np.reshape(m, (dim, nr_mixtures))
 
     datasets = []
     datasets.append('enroll_stats')
@@ -138,10 +156,10 @@ def main():
     for dataset in datasets:
         list_file = "{}/{}.lst".format(LISTS_PATH, dataset)
         spk_logical, spk_physical = parse_list(list_file)
-        n_sessions = len(spk_logical)
+        nr_sessions = len(spk_logical)
 
-        N = np.empty((n_sessions, n_mixtures))
-        F = np.empty((n_sessions, n_mixtures))
+        N = np.empty((nr_sessions, nr_mixtures))
+        F = np.empty((nr_sessions, nr_mixtures))
 
         for i in range(len(spk_physical)):
             session_file = "{}/{}.ascii".format(JFA_PATH, spk_physical[i])
@@ -165,7 +183,7 @@ def main():
 # follow http://www1.icsi.berkeley.edu/Speech/presentations/AFRL_ICSI_visit2_JFA_tutorial_icsitalk.pdf
 
 # 2/ GMM Training
-    n_gaussians = 256
+    nr_gaussians = 256
     iterk = 25
     iterg_train = 25
     end_acc = 0.0001
@@ -175,10 +193,10 @@ def main():
     update_variances = True
     norm_KMeans = True
 
-    for gaussian_n in n_gaussians:
-        print "training", gaussian_n
-        print "merging", gaussian_n
-        C = np.array(n_gaussians)
+    for gaussianr_n in nr_gaussians:
+        print "training", gaussianr_n
+        print "merging", gaussianr_n
+        C = np.array(nr_gaussians)
         F = np.array(acusitc_features_vector_size)
 
         m = GMM(M, D, features)
@@ -200,8 +218,8 @@ def main():
 
     ru = 50 # The dimensionality of the subspace
     relevance_factor = 4
-    n_iter_train = 10
-    n_iter_enrol = 1
+    nr_iter_train = 10
+    nr_iter_enrol = 1
 
     for M in Ms:
         print "M", M
@@ -222,7 +240,7 @@ def main():
     responsibilities_threshold = 0
 """
 
-if "__main__" == __name__:
+if "__mainr__" == __name__:
     main()
 else:
     print "imported, quitting"
